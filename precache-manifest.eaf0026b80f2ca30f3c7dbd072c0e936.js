c0f99730168f73e1f43172f0d1c1ccfd122756c2
Zainab <85569752+zainab1981@users.noreply.github.com>
2021-06-13 16:52:44 +0530

    Add files via upload

---
+++ precache-manifest.eaf0026b80f2ca30f3c7dbd072c0e936.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d459c295e04a8eef8c2a19d530d44ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0861e2130b2bc2e5b045",
+    "url": "/static/css/2.14351861.chunk.css"
+  },
+  {
+    "revision": "40378d60ae8f6ac9db4b",
+    "url": "/static/css/main.c3c6b22d.chunk.css"
+  },
+  {
+    "revision": "0861e2130b2bc2e5b045",
+    "url": "/static/js/2.9ea309d0.chunk.js"
+  },
+  {
+    "revision": "76c33606446526c6ec38bc107ec65a87",
+    "url": "/static/js/2.9ea309d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "40378d60ae8f6ac9db4b",
+    "url": "/static/js/main.60b62db0.chunk.js"
+  },
+  {
+    "revision": "79d439c7be0dd499b205",
+    "url": "/static/js/runtime-main.e9933245.js"
+  },
+  {
+    "revision": "38828042bce3def5dbc3c5b4a5d2df93",
+    "url": "/static/media/Pencil.38828042.jpg"
+  },
+  {
+    "revision": "d4f5eb2c95a3417dc8c809d8eeda5cb4",
+    "url": "/static/media/Z LOGO.d4f5eb2c.jpg"
+  },
+  {
+    "revision": "5c72e21ea2a487dc3e3175f964da0721",
+    "url": "/static/media/Zain.5c72e21e.jpg"
+  },
+  {
+    "revision": "87561258a0fb17472008a4b2ff66696f",
+    "url": "/static/media/laptop1.87561258.jpg"
+  }
+]);
